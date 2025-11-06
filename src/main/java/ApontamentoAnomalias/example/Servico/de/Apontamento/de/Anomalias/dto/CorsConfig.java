056d4cd848b8605e8b0ad4c1cdac3208b0176ae2
Manoel <125422009+ManoelMorais@users.noreply.github.com>
2025-11-06 09:34:13 -0300

    teste

--- src/main/java/ApontamentoAnomalias/example/Servico/de/Apontamento/de/Anomalias/dto/CorsConfig.java
+++ src/main/java/ApontamentoAnomalias/example/Servico/de/Apontamento/de/Anomalias/dto/CorsConfig.java
@@ -10,7 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("""
+                        https://servico-de-apontamento-de-anomalias.onrender.com
+                        """)
                 .allowedMethods("GET", "POST", "DELETE", "PUT")
                 .allowedHeaders("*") // <- permite qualquer cabeçalho
                 .allowCredentials(true); // <- permite envio de cookies/autenticação se necessário
